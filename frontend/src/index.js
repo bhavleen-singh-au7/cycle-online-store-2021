4c8d067801c3ffce4740cc340213a3ee8fe687bf
Bhavleen <singhbhavleen3@gmail.com>
2021-01-10 22:02:58 +0530

    Updated Styles

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+import Routes from "./Routes";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Routes />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
