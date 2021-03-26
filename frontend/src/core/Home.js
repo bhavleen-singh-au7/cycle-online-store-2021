23fc158c17971e2e069104e5e66fd40defac89ef
Bhavleen <singhbhavleen3@gmail.com>
2021-03-26 20:25:20 +0530

    Home files in EComm

--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -1,22 +1,43 @@
-import { API } from "../backend";
-import Base from "./index";
+import React, { useEffect, useState } from "react";
+import "../styles.css";
+import Base from "./Base";
+import Card from "./Card";
+import { getProducts } from "./helper/coreapicalls";
 
 export default function Home() {
-  console.log("API IS", API);
+  const [products, setProducts] = useState([]);
+  const [setError] = useState(false);
+
+  const loadAllProduct = () => {
+    getProducts()
+      .then((data) => {
+        if (data.error) {
+          setError(data.error);
+        } else {
+          setProducts(data);
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    loadAllProduct();
+  }, []);
 
   return (
     <Base
       title="Home Page"
       description="Welcome to the Tshirt Store">
-      <div className="row">
-        <div className="col-4">
-          <button className="btn btn-success">TEST</button>
-        </div>
-        <div className="col-4">
-          <button className="btn btn-success">TEST</button>
-        </div>
-        <div className="col-4">
-          <button className="btn btn-success">TEST</button>
+      <div className="row text-center">
+        <h1 className="text-white">All of Cycles</h1>
+        <div className="row">
+          {products.map((product, index) => {
+            return (
+              <div key={index} className="col-4 mb-4">
+                <Card product={product} />
+              </div>
+            );
+          })}
         </div>
       </div>
     </Base>
